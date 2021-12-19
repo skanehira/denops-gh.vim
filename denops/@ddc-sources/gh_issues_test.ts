987a827a89fac0cf73b86a4bb870e4732aa5a10e
skanehira <sho19921005@gmail.com>
2021-12-19 13:09:48 +0900

    feature: autocomplete user name when edit issue.

--- denops/@ddc-sources/gh_issues_test.ts
+++ denops/@ddc-sources/gh_issues_test.ts
@@ -1,13 +1,13 @@
 import { Candidate } from "https://deno.land/x/ddc_vim@v0.15.0/types.ts#^";
-import { getCandidates, issueCache } from "./gh_issues.ts";
+import { getCandidates, issueCache, userCache } from "./gh_issues.ts";
 import { Denops, path, test } from "../gh/deps.ts";
 import { setActionCtx } from "../gh/action.ts";
 import { assertEqualFile, parseJSON } from "../gh/utils/test.ts";
-import { IssueItem } from "../gh/github/schema.ts";
+import { IssueItem, User } from "../gh/github/schema.ts";
 
 test({
   mode: "all",
-  name: "autocomplete from github api",
+  name: "autocomplete issue from github api",
   fn: async (denops: Denops) => {
     try {
       await setActionCtx(denops, {
@@ -19,7 +19,7 @@ test({
         "denops",
         "@ddc-sources",
         "testdata",
-        "want_candidate_list.json",
+        "want_candidate_issue_list.json",
       );
 
       assertEqualFile(file, actual);
@@ -31,14 +31,14 @@ test({
 
 test({
   mode: "all",
-  name: "autocomplete from cache",
+  name: "autocomplete issue from cache",
   fn: async (denops: Denops) => {
     try {
       const cacheFile = path.join(
         "denops",
         "@ddc-sources",
         "testdata",
-        "want_candidate_list.json",
+        "want_candidate_issue_list.json",
       );
 
       const candidates = await parseJSON<Candidate<IssueItem>[]>(cacheFile);
@@ -55,12 +55,72 @@ test({
         "denops",
         "@ddc-sources",
         "testdata",
-        "want_candidate_list.json",
+        "want_candidate_issue_list.json",
       );
 
       assertEqualFile(file, actual);
     } finally {
       issueCache.clear();
+    }
+  },
+});
+
+test({
+  mode: "all",
+  name: "autocomplete user from github api",
+  fn: async (denops: Denops) => {
+    try {
+      await setActionCtx(denops, {
+        schema: { owner: "skanehira", repo: "test", actionType: "issues:list" },
+      });
+      const actual = await getCandidates(denops, "@s");
+
+      const file = path.join(
+        "denops",
+        "@ddc-sources",
+        "testdata",
+        "want_candidate_user_list.json",
+      );
+
+      assertEqualFile(file, actual);
+    } finally {
+      issueCache.clear();
+    }
+  },
+});
+
+test({
+  mode: "all",
+  name: "autocomplete user from cache",
+  fn: async (denops: Denops) => {
+    try {
+      const cacheFile = path.join(
+        "denops",
+        "@ddc-sources",
+        "testdata",
+        "want_candidate_user_list.json",
+      );
+
+      const candidates = await parseJSON<Candidate<User>[]>(cacheFile);
+      for (const c of candidates) {
+        userCache.set(c.word, c);
+      }
+
+      await setActionCtx(denops, {
+        schema: { owner: "skanehira", repo: "test", actionType: "issues:list" },
+      });
+      const actual = await getCandidates(denops, "@s");
+
+      const file = path.join(
+        "denops",
+        "@ddc-sources",
+        "testdata",
+        "want_candidate_user_list.json",
+      );
+
+      assertEqualFile(file, actual);
+    } finally {
+      userCache.clear();
     }
   },
 });
