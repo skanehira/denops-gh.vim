b3d5be8b8ef18c161f1367747fb5e1a59a1935c3
skanehira <sho19921005@gmail.com>
2021-12-29 00:08:26 +0900

    refactor: add test that update issue

--- denops/gh/github/issue_test.ts
+++ denops/gh/github/issue_test.ts
@@ -1,4 +1,4 @@
-import { getIssues } from "./issue.ts";
+import { getIssues, updateIssue } from "./issue.ts";
 import { path } from "../deps.ts";
 import { assertEqualFile } from "../utils/test.ts";
 
@@ -22,5 +22,36 @@ Deno.test({
       "want_issue_list.json",
     );
     await assertEqualFile(file, actual);
+  },
+});
+
+Deno.test({
+  name: "update issue",
+  fn: async () => {
+    try {
+      const actual = await updateIssue({
+        input: {
+          id: "MDU6SXNzdWU3MDk3MzE0NTA=",
+          state: "OPEN",
+        },
+      });
+
+      const file = path.join(
+        "denops",
+        "gh",
+        "github",
+        "testdata",
+        "want_update_issue_result.json",
+      );
+      await assertEqualFile(file, actual);
+    } finally {
+      // restore labels
+      await updateIssue({
+        input: {
+          id: "MDU6SXNzdWU3MDk3MzE0NTA=",
+          state: "CLOSED",
+        },
+      });
+    }
   },
 });
