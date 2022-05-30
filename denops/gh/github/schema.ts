8db0657c8539c48152be5ed31773b6bbf38418aa
skanehira <sho19921005@gmail.com>
2022-05-30 20:19:42 +0900

    refactor: add issue comment type

--- denops/gh/github/schema.ts
+++ denops/gh/github/schema.ts
@@ -23,7 +23,52 @@ export const isIssueBody = (arg: unknown): arg is IssueBodyFragment => {
 };
 
 export type IssueComment = {
+  url: string;
+  html_url: string;
+  issue_url: string;
+  id: number;
+  node_id: string;
+  user: User;
+  created_at: string;
+  updated_at: string;
+  author_association: string;
   body: string;
+  reactions: Reactions;
+  performed_via_github_app?: null;
+};
+
+type User = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+};
+
+type Reactions = {
+  url: string;
+  total_count: number;
+  "+1": number;
+  "-1": number;
+  laugh: number;
+  hooray: number;
+  confused: number;
+  heart: number;
+  rocket: number;
+  eyes: number;
 };
 
 export const isIssueComment = (arg: unknown): arg is IssueComment => {
